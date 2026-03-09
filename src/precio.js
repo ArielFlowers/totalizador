f25b074df7e2e96d4e77f341b1fc9a182a06c3f5
ArielFlowers <arielmakotoflores@gmail.com>
2026-03-09 13:50:17 -0400

    feat: calcular envio  por peso volumetrico corregido

--- src/precio.js
+++ src/precio.js
@@ -48,7 +48,11 @@ export function aplicarCategoria(total, categoria) {
 }
 
 export function calcularEnvio(peso) {
+    if (peso <= 10) return 0;
     if (peso <= 20) return 3.5;
+    if (peso <= 40) return 5;
     if (peso <= 80) return 6;
+    if (peso <= 100) return 6.5;
+    if (peso <= 200) return 8;
     return 9;
 }
