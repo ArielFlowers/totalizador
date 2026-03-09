c62816a9c4cea2ce390d720a0b0d3a9bf1ef4eaf
ArielFlowers <arielmakotoflores@gmail.com>
2026-03-09 11:22:10 -0400

    feat: le puse nombre descriptivo al test

--- src/precio.test.js
+++ src/precio.test.js
@@ -1,5 +1,7 @@
 import { calcularSubtotal } from "./precio.js";
 
-it("debería calcular correctamente el subtotal", () => {
-  expect(calcularSubtotal(2, 50)).toBe(100);
+describe("Cálculo del subtotal", () => {
+    it("debería calcular correctamente el subtotal", () => {
+      expect(calcularSubtotal(2, 50)).toBe(100);
+    });
 });
